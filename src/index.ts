83b5b9380622675cb4928a87baf24644bb2e4b5e
jmarrmd <91326068+jmarrmd@users.noreply.github.com>
2026-03-29 14:08:23 -0600

    Delete src directory

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { API } from 'homebridge';
-import { RaypakPlatform } from './platform';
-import { PLATFORM_NAME } from './settings';
-
-export = (api: API): void => {
-  api.registerPlatform(PLATFORM_NAME, RaypakPlatform);
-};
--- src/settings.ts
+++
@@ -1,2 +0,0 @@
-export const PLATFORM_NAME = 'RaypakPoolHeater';
-export const PLUGIN_NAME = 'homebridge-raypak';
