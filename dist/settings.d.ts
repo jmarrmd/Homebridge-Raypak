e001b8e91b501d85e90e1076649664b0d05f0e2f
jmarrmd <91326068+jmarrmd@users.noreply.github.com>
2026-03-29 14:08:11 -0600

    Delete dist directory

--- dist/settings.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const PLATFORM_NAME = "RaypakPoolHeater";
-export declare const PLUGIN_NAME = "homebridge-raypak";
-//# sourceMappingURL=settings.d.ts.map
--- dist/settings.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PLUGIN_NAME = exports.PLATFORM_NAME = void 0;
-exports.PLATFORM_NAME = 'RaypakPoolHeater';
-exports.PLUGIN_NAME = 'homebridge-raypak';
-//# sourceMappingURL=settings.js.map
